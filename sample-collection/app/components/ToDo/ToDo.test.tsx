c181aa7d245e30c77bbb775ce27973ee7282c02f
takeshi.fujii <isisaka3000@gmail.com>
2024-08-28 13:52:49 +0900

    不要モジュールのimport削除

--- sample-collection/app/components/ToDo/ToDo.test.tsx
+++ sample-collection/app/components/ToDo/ToDo.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom/jest-globals';
 import '@testing-library/jest-dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ToDo from './ToDo';
